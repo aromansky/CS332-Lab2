084f74cbff951303487f96ed52ce53f2d2b2c455
Semechka <115107268+utsushka@users.noreply.github.com>
2025-09-24 12:24:17 +0300

    Доделал задание

--- Task2.cs
+++ Task2.cs
@@ -12,6 +12,9 @@ namespace CS332_Lab2
     public static class Task2
     {
         public enum COLOR { R, G, B };
+        public static MyImage RImage = null;
+        public static MyImage GImage = null;
+        public static MyImage BImage = null;
         public static void updateDict<T>(Dictionary<T, int> dict, T key)
         {
             if (dict.ContainsKey(key))
@@ -27,20 +30,36 @@ namespace CS332_Lab2
         public static Dictionary<(COLOR, int), int> extractRGB(MyImage img)
         {
             Dictionary<(COLOR, int), int> res = new Dictionary<(COLOR, int), int>();
-            img.Lock();
+            RImage = new MyImage(new Bitmap(img.Img));
+            GImage = new MyImage(new Bitmap(img.Img));
+            BImage = new MyImage(new Bitmap(img.Img));
 
-            for(int x = 0; x < img.Width; x++)
+            img.Lock();
+            RImage.Lock();
+            GImage.Lock();
+            BImage.Lock();
+
+            for (int x = 0; x < img.Width; x++)
             {
                 for(int y = 0; y < img.Height; y++)
                 {
                     var (r, g, b) = img.GetRGB(x, y);
                     updateDict(res, (COLOR.R, r));
+                    RImage.SetPixel(x, y, (r, 0, 0));
+
                     updateDict(res, (COLOR.G, g));
+                    GImage.SetPixel(x, y, (0, g, 0));
+
                     updateDict(res, (COLOR.B, b));
+                    BImage.SetPixel(x, y, (0, 0, b));
                 }
             }
 
             img.Unlock();
+            RImage.Unlock();
+            GImage.Unlock();
+            BImage.Unlock();
+
             return res;
         }
     }
